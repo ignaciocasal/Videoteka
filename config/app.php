e6b05a5abf8337f44d922cf356f55a7c6245a8f1
ignaciocasal <ignaciocasaldev@gmail.com>
2017-07-12 00:56:36 +0000

    config files

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // My Providers
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
