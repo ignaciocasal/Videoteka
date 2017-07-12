62e04df4d8b80ea4fb2cf0a2f23dc1e93386b715
DESKTOP-0ERQ0F8\Lucas <lucascasas095@gmail.com>
2017-07-12 10:45:16 -0300

    Rutas
    Se agregó archivo de rutas

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,31 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::group(['prefix' => 'admin','middleware' => 'auth'],function(){
+  Route::resource('users','UsersController'); //Primer parametro: nombre de grupo de rutas / segundo nombre del controlador
+    Route::get('users/{id}/destroy', [
+      'uses' => 'UsersController@destroy',
+      'as' => 'admin.users.destroy'
+    ]);
+
+  Route::resource('movies','MoviesController');
+    Route::get('movies/{id}/destroy',[
+      'uses' => 'MoviesController@destroy',
+      'as' => 'admin.movies.destroy'
+    ]);
+  Route::resource('rents','RentsController');
+    Route::get('rents/{id}/destroy',[
+      'uses' => 'RentsController@destroy',
+      'as' => 'admin.rents.destroy'
+    ]);
+  Route::resource('genres','GenresController');
+  Route::get('genres/{id}/destroy', [
+      'uses'  => 'GenresController@destroy',
+        'as'    => 'admin.genres.destroy'
+  ]);
+
 });
