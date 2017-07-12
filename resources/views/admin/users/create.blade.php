74a13fb440cbb90d3b17fcb7455db1dc0f566cfa
ignaciocasal <ignaciocasaldev@gmail.com>
2017-07-12 11:38:06 +0000

    Changes in users create view

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -6,12 +6,27 @@
     {!! Form::open(['route'=>'users.store', 'method'=>'POST']) !!}
     <div class="form-group">
         {!! Form::label('name','Nombre') !!}
-        {!! Form::text('name', null, ['class'=>'form-control', 'placeholder'=>'Nombre Completo']) !!}
+        {!! Form::text('name', null, ['class'=>'form-control', 'placeholder'=>'Nombre']) !!}
     </div>
 
     <div class="form-group">
-        {!! Form::label('email','Email') !!}
+        {!! Form::label('lastname','Apellido') !!}
+        {!! Form::text('lastname', null, ['class'=>'form-control', 'placeholder'=>'Apellido']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('dni','DNI') !!}
+        {!! Form::text('dni', null, ['class'=>'form-control', 'placeholder'=>'Documento de Identidad']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('email','E-mail') !!}
         {!! Form::email('email', null, ['class'=>'form-control', 'placeholder'=>'Correo Electronico']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('phone','Teléfono') !!}
+        {!! Form::text('phone', null, ['class'=>'form-control', 'placeholder'=>'Número de teléfono']) !!}
     </div>
 
     <div class="form-group">
@@ -21,7 +36,7 @@
 
     <div class="form-group">
         {!! Form::label('type','Tipo') !!}
-        {!! Form::select('type', ['member'=>'Miembro','admin' =>'Administrador'], null, ['class'=>'form-control']) !!}
+        {!! Form::select('type', ['member'=>'Socio','admin' =>'Administrador'], null, ['class'=>'form-control']) !!}
     </div>
 
     <div class="form-group">
