40364ccf855b3658707656ea62e39aad2db51582
ignaciocasal <ignaciocasaldev@gmail.com>
2017-07-12 00:56:19 +0000

    Added migrations

--- database/migrations/2017_07_11_000002_create_genres_table.php
+++ database/migrations/2017_07_11_000002_create_genres_table.php
@@ -3,17 +3,17 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGenresTable extends Migration
 {
     /**
      * Schema table name to migrate
      * @var string
      */
-    public $set_schema_table = 'users';
+    public $set_schema_table = 'genres';
 
     /**
      * Run the migrations.
-     * @table users
+     * @table genres
      *
      * @return void
      */
@@ -23,14 +23,8 @@ class CreateUsersTable extends Migration
             $table->engine = 'InnoDB';
             $table->increments('id');
             $table->string('name', 45);
-            $table->string('lastname', 45);
-            $table->string('dni', 45)->unique();
-            $table->string('email', 45)->unique();
-            $table->string('phone', 45);
-            $table->string('password', 45);
-            $table->string('type', 45);
-            $table->softDeletes();
 
+            $table->softDeletes();
         });
     }
 
