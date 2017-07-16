d5fa820d02ccaab00c22856563d9286bfac3f6d1
ignaciocasal <ignaciocasaldev@gmail.com>
2017-07-16 12:18:23 +0000

    Unnecessary comments removed in Aside view

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -1,15 +1,6 @@
 <!-- Search Well -->
 <div class="well">
     <h4>{{ __('messages.search') }}</h4>
-    {{--<div class="input-group">
-        <input type="text" class="form-control">
-        <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
-                                <span class="glyphicon glyphicon-search"></span>
-                        </button>
-                        </span>
-    </div>
-    <!-- /.input-group -->--}}
     <div class="input-group">
         {!!   Form::open(['route' => 'home.index', 'method' => 'GET', 'class' => 'navbar-form navbar-right', 'role' => 'search']) !!}
         <div class="form-group">
@@ -18,13 +9,7 @@
         <button type="submit" class="btn btn-default"><i class="fa fa-search" aria-hidden="true"></i></button>
         {!! Form::close() !!}
     </div>
-
-
-
 </div>
-
-
-
 
 
 <!-- Genres Well -->
@@ -49,30 +34,4 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
 </div>--}}
 
-@section('js')
-    {{--<script>
-        $(document).ready(function(){
-            // add button style
-            $(".poll_bar").addClass("btn btn-default");
-            // Add button style with alignment to left with margin.
-            $(".poll_bar").css({"text-align":"left","margin":"5px"});
-
-            //loop
-            $(".poll_bar").each(
-                function(i){
-                    //get poll value
-                    var bar_width = (parseFloat($(".poll_val").eq(i).text())/1.3).toString();
-                    bar_width = bar_width + "%"; //add percentage sign.
-                    //set bar button width as per poll value mention in span.
-                    $(".poll_bar").eq(i).width(bar_width);
-
-                    //Define rules.
-                    var bar_width_rule = parseFloat($(".poll_val").eq(i).text());
-                    if(bar_width_rule >= 50){$(".poll_bar").eq(i).addClass("btn btn-sm btn-success")}
-                    if(bar_width_rule <  50){$(".poll_bar").eq(i).addClass("btn btn-sm btn-warning")}
-                    if(bar_width_rule <= 10){$(".poll_bar").eq(i).addClass("btn btn-sm btn-danger")}
-                });
-        });
-    </script>--}}
-@endsection
 
