00cbc6173bb289ba484b0ea1ba3496d101189d59
ignaciocasal <ignaciocasaldev@gmail.com>
2017-07-18 20:23:44 +0000

    Changed app name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title') - {{  config('app.name', 'Blog Laravel') }}</title>
+    <title>@yield('title') - {{  config('app.name') }}</title>
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('plugins/bootstrap/css/bootstrap.min.css') }}">
